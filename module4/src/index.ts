b4d6ed642a1cd9f20ca9f5f92be7ff4daf4890fd
Labib361 <ahsanulmahbub1999@gmail.com>
2025-05-08 09:53:49 +0600

    console.log deleted

--- module4/src/index.ts
+++ module4/src/index.ts
@@ -6,20 +6,12 @@ function formatString(input: string, toUpper?: boolean): string {
         return input.toUpperCase();
     }
   }
-  console.log(formatString("Hello"));          
-console.log(formatString("Hello", true));   
-console.log(formatString("Hello", false));  
+    
 
 function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[] {
     return items.filter(item => item.rating >= 4);
   }
-  const books = [
-    { title: "Book A", rating: 4.5 },
-    { title: "Book B", rating: 3.2 },
-    { title: "Book C", rating: 5.0 }
-  ];
-  
-  console.log(filterByRating(books));
+ 
 
   function concatenateArrays<T>(...arrays: T[][]): T[] {
     let output: T[] = [];
@@ -28,8 +20,7 @@ function filterByRating(items: { title: string; rating: number }[]): { title: st
     }
     return output;
   }
- console.log(concatenateArrays(["a", "b"], ["c"])) ; 
- console.log(concatenateArrays([1, 2], [3, 4], [5]));  
+ 
  
  class Vehicle {
     private make: string;
@@ -57,9 +48,7 @@ function filterByRating(items: { title: string; rating: number }[]): { title: st
       return `Model: ${this.model}`;
     }
   }
-  const myCar = new Car("Toyota", 2020, "Corolla");
-  console.log(myCar.getInfo());   
-  console.log(myCar.getModel());  
+ 
 
   function processValue(value: string | number): number {
     if (typeof value === "string") {
@@ -67,15 +56,14 @@ function filterByRating(items: { title: string; rating: number }[]): { title: st
     } else {
       return value * 2;
     }
-  }
-  console.log(processValue("hello"));
-console.log(processValue(10));   
+  }  
+
 interface Product {
     name: string;
     price: number;
   }
-  
   function getMostExpensiveProduct(products: Product[]): Product | null {
+  
     if (products.length === 0) {
       return null;
     }
@@ -90,13 +78,7 @@ interface Product {
   
     return mostExpensive;
   }
-  const products = [
-    { name: "Pen", price: 10 },
-    { name: "Notebook", price: 25 },
-    { name: "Bag", price: 50 }
-  ];
  
-  console.log(getMostExpensiveProduct(products));
   enum Day {
     Monday,
     Tuesday,
@@ -116,8 +98,7 @@ interface Product {
         return "Weekday";
     }
   }
-  console.log(getDayType(Day.Monday));   // Output: "Weekday"
-console.log(getDayType(Day.Saturday)); // Output: "Weekend"
+
 
 async function squareAsync(n: number): Promise<number> {
     return new Promise((resolve, reject) => {
@@ -130,11 +111,5 @@ async function squareAsync(n: number): Promise<number> {
       }, 1000);
     });
   }
-  squareAsync(4)
-  .then(result => console.log(result)) // Output after 1 second: 16
-  .catch(error => console.error(error));
-
-squareAsync(-2)
-  .then(result => console.log(result))
-  .catch(error => console.error(error.message));   
+ 
    
